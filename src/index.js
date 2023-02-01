db59aa60165bec7cb455722e3a9fd673a90be09b
adnansoffiatti <79457983+adnansoffiatti@users.noreply.github.com>
2023-02-01 08:27:15 -0300

    Configurando banco de dados

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const PORT = process.env.PORT || 3333;
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.listen(PORT, () => console.log('Server running'));
+const connectToDatabase = async () => {
+    mongoose.set('strictQuery', false);
+
+    await mongoose.connect(process.env.MONGO_URI, console.log("Connected to database"));
+}
+connectToDatabase();
+
+app.listen(PORT, () => console.log("Server running"));
