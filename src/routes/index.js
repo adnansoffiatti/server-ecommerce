cdee72f3d8cb1d7368d1ebd1de1447b70487dbc8
adnansoffiatti <79457983+adnansoffiatti@users.noreply.github.com>
2023-02-01 08:47:18 -0300

    Configurando rotas do projeto

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,43 @@ routes.get("/", (req, res) => {
     res.send("Olá mundinho lindo!");
 });
 
+// rota para criar usuário
+routes.post('/users');
+
+// rota para listar usuários
+routes.get('/users');
+
+// rota para listar usuário específico
+routes.get('/users/:user_id');
+
+// rota para fazer login
+routes.post('/login');
+
+// rota para criar produto
+routes.post('/products/:user_id');
+
+// rota para listar os produtos de um usuário
+routes.get('/products/:user_id');
+
+// rota para atualizar o produto
+routes.patch('/products/:user_id/:product_id');
+
+// rota para deletar produtos
+routes.delete('/products/:user_id/product_id');
+
+// rota para listar todos os produtos
+routes.get('/products');
+
+// rota para buscar produto específico
+routes.get('/products/:product_id');
+
+// rota para listar carrinho
+routes.post('/cart/:user_id');
+
+// todos os carts de um usuário
+routes.get('/cart/:user_id');
+
+// rota para listar carrinho específico
+routes.get('/cart/:user_id/:card_id');
+
 module.exports = routes;
